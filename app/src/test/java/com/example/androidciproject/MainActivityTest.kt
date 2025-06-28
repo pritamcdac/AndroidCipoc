1bd8c352e92720013f1c5dab63fa1ed2ab1a5a2c
pritamcdac <pritamde@cybage.com>
2025-06-29 01:19:09 +0530

    Adding Tests

---
+++ app/src/test/java/com/example/androidciproject/MainActivityTest.kt
@@ -0,0 +1,18 @@
+package com.example.androidciproject
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class MainActivityTest {
+
+    @Test
+    fun testAppName() {
+        val appName = "AndroidCIProject"
+        assertEquals("AndroidCIProject", appName)
+    }
+
+    @Test
+    fun dummyTest_passes() {
+        assertEquals(4, 2 + 2)
+    }
+}
