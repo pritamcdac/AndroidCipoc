cbf44320c01c6f110835062bd6adde2cc5d5b0fb
pritamde@cybage <93193350+pritamcdac@users.noreply.github.com>
2025-08-11 18:14:12 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,4 +33,9 @@ android {
     buildFeatures {
         viewBinding = true
     }
+
+   dependencies {
+    implementation("androidx.appcompat:appcompat:1.7.0") // latest stable
+    implementation("com.google.android.material:material:1.12.0") // Material Components
+}
 }
