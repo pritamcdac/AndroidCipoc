06769948f9f5e6434842a9248e8b19bd47860ccb
pritamde@cybage <93193350+pritamcdac@users.noreply.github.com>
2025-08-11 18:03:34 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,23 +4,15 @@ plugins {
 }
 
 android {
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-}
-
-kotlin {
-    jvmToolchain(17)
-}
-
+    namespace = "com.cybage.androidciproject"
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.cybage.androidciproject"
         minSdk = 24
         targetSdk = 34
         versionCode = 2
-        versionName = "1.1.0"
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -41,15 +33,4 @@ kotlin {
     buildFeatures {
         viewBinding = true
     }
-}
-
-dependencies {
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
